a4143bfc565b17e63f98d33765cafcd330fabb59
netivan <72153714+netivan@users.noreply.github.com>
2020-10-01 17:04:38 +0200

    Add project files.

---
+++ övning.7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace övning._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proviamo github");
+        }
+    }
+}
